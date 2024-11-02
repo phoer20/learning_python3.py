0f8059980054925abb02347800645d9387eee8a5
Nik⭕️las <93901949+phoer20@users.noreply.github.com>
2024-11-01 22:18:33 -0700

    Create for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,12 @@
+allow_list = ["192.168.243.140", "192.168.205.12", "192.168.151.162", "192.168.178.71", 
+              "192.168.86.232", "192.168.3.24", "192.168.170.243", "192.168.119.173"]
+
+ip_addresses = ["192.168.142.245", "192.168.109.50", "192.168.86.232", "192.168.131.147",
+                "192.168.205.12", "192.168.200.48"]
+
+for i in ip_addresses:
+	if i in allow_list:
+		print("IP address is allowed")
+	else:
+		print("IP address is not allowed. Further investigation of login activity required")
+		break
