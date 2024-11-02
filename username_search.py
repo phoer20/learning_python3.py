22e08af978fd0581e09c75aba7a1e44a553d0fcb
Nik⭕️las <93901949+phoer20@users.noreply.github.com>
2024-11-01 22:30:52 -0700

    Create username_search.py

---
+++ username_search.py
@@ -0,0 +1,6 @@
+#Extract username and login attempts without ID number or department
+
+import re
+pattern = "\w+:\s\d+"
+employee_logins_string = "1001 bmoreno: 12 Marketing 1002 tshah: 7 Human Resources 1003 sgilmore: 5 Finance"
+print(re.findall(pattern, employee_logins_string))
